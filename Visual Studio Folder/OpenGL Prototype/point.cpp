4219684542d51b9115db26eb32776b618f08a289
Kayne Ruse <ratstail91@yahoo.com.au>
2012-06-02 17:43:48 +1000

    Updated the comment headers.

--- Visual Studio Folder/OpenGL Prototype/point.cpp
+++ Visual Studio Folder/OpenGL Prototype/point.cpp
@@ -5,18 +5,18 @@
  * 
  * This file is part of Project Meteor.
  * 
- * Project RPG is free software: you can redistribute it and/or modify
+ * Project Meteor is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * Project RPG is distributed in the hope that it will be useful,
+ * Project Meteor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Project RPG.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Project Meteor.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * Description: 
  *     A single point in three dimensional space.
